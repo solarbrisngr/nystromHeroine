dce694ea846a362894cbbb12b878f3ffc2b45ae0
solarbrisngr <59808313+solarbrisngr@users.noreply.github.com>
2020-02-21 21:11:03 -0700

    Add files via upload

---
+++ Scripts/JumpingPlayerState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingPlayerState : IPlayerState
+{
+    Player mPlayer;
+    Rigidbody rbPlayer;
+
+    // Start is called before the first frame update
+    public void Enter(Player player)
+    {
+        //Debug.Log("Entering State: Jumping");
+        rbPlayer = player.GetComponent<Rigidbody>();
+        rbPlayer.AddForce(0, 400 * Time.deltaTime, 0, ForceMode.VelocityChange);
+        player.mCurrentState = this;
+    }
+
+    public void Execute(Player player)
+    {
+        if (Physics.Raycast(rbPlayer.transform.position, Vector3.down, 0.55f))
+        {
+            StandingPlayerState standingState = new StandingPlayerState();
+            standingState.Enter(player);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            DuckingPlayerState duckState = new DuckingPlayerState();
+            rbPlayer.AddForce(0, -400 * Time.deltaTime, 0, ForceMode.VelocityChange);
+            duckState.Enter(player);
+        }
+    }
+}
